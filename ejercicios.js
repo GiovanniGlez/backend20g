e46f6bb72c66c8690eb6ed36dc7adfa194105dec
Gio <giovannigonzalezdelgado25@gmail.com>
2022-08-09 20:43:38 -0500

    Ejercicio con reduce,package y spread operator

--- ejercicios.js
+++ ejercicios.js
@@ -47,7 +47,8 @@ console.log("Nombres largos", largeNames)
 
 // Ejercicio 2
 
-// Hacer una funcion que se llame transformarNombres reciba como parametro un array de nombres y regresar un objeto de objetos que contenga el nombre, el length del nombre y si es mayor a 4 o no
+// Hacer una funcion que se llame transformarNombres reciba como parametro un array de nombres 
+//y regresar un objeto de objetos que contenga el nombre, el length del nombre y si es mayor a 4 o no
 // Input: ["Ale", "Mariana", "Adrian"]...
 // Output:
 // {
@@ -59,3 +60,34 @@ console.log("Nombres largos", largeNames)
 // }
 
 // reduce -> cambiar datos
+
+// const transformarNombres = (names) => {
+//     let result = {}
+//     names.reduce((acc,cv,i) => {
+//         return result[i] = {
+//             name: cv,
+//             length: cv.length,
+//             isGreaterThanFour: `${cv.length >3 ? true : false}`
+//         }
+//     },'')
+//     return result
+// }
+
+// console.log(transformarNombres(nombres))
+
+const transformNames = (names) => {
+    const koders = {} //vacio
+
+    names.forEach((valor, indice) =>{
+        koders[indice] = {
+            name: valor,
+            length: valor.length,
+            isGreaterThanFour: valor.length > 3
+        }
+    })
+    return koders
+}
+
+const transformedNames = transformNames(nombres)
+console.log("transformedNames", transformedNames)
+
