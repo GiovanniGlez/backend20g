dd7e19daf084ceb2bede0f5a7a62ba46ee6fc1bd
Gio <giovannigonzalezdelgado25@gmail.com>
2022-08-10 19:13:54 -0500

    se realiza ejercicio de readFile

--- fileSystem.js
+++ fileSystem.js
@@ -1,6 +1,6 @@
 const fs = require("fs")
 
-console.log("fs write", fs.writeFile)
+console.log("fs write", fs.writeFile, "fs read", fs.readFile)
 
 fs.writeFile("text1.txt", "Archivo creado desde node", "utf8", (err) =>{
     //Logica negativa
@@ -10,4 +10,14 @@ fs.writeFile("text1.txt", "Archivo creado desde node", "utf8", (err) =>{
     }
     //Si no, todo lo demas
     console.log("el archivo fue un exito")
+})
+
+fs.readFile('text.txt', 'utf-8', (err, data) => {
+    //negando el error
+    if(!err){
+        console.log(data);
+        //Error
+    } else {
+        console.log("Error", err);
+    }
 })
